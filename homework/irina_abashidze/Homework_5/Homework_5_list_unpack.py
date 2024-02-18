9ff1876f0f635ac43986b06e5350ed72541325b1
Irina Abashidze <irina.abashidze@gmail.com>
2024-02-18 13:19:04 +0100

    Homework_5 modified file Homework_5_list_unpack.py

--- homework/irina_abashidze/Homework_5/Homework_5_list_unpack.py
+++ homework/irina_abashidze/Homework_5/Homework_5_list_unpack.py
@@ -9,12 +9,18 @@ result2 = 'результат операции: 514'
 result3 = 'результат работы программы: 9'
 # print(result1.index('42'))
 # print(result1[-2:])
-print(int(result1[-2:]) + 10)
+# print(int(result1[-2:]) + 10)
 # print(type(int(result2[-3:])))
 # print(result2[-3:])
-print(int(result2[-3:]) + 10)
+# print(int(result2[-3:]) + 10)
 # print(result3[-1])
-print(int(result3[-1]) + 10)
+# print(int(result3[-1]) + 10)
+FirstResult = result1.split(":")
+print(int(FirstResult[1]) + 10)
+SecondResult = result2.split(":")
+print(int(SecondResult[1]) + 10)
+ThirdResult = result3.split(":")
+print(int(ThirdResult[1]) + 10)
 
 students = ['Ivanov', 'Petrov', 'Sidorov']
 # students = ', '.join(students)
