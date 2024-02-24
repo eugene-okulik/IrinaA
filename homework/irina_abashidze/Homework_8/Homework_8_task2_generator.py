c6befced04873cea449c0b9b713a480f7d21aced
Irina Abashidze <irina.abashidze@gmail.com>
2024-02-24 09:41:51 +0100

    Modiffied file Homework_8_task2_generator.py

--- homework/irina_abashidze/Homework_8/Homework_8_task2_generator.py
+++ homework/irina_abashidze/Homework_8/Homework_8_task2_generator.py
@@ -52,3 +52,35 @@ print("Пятое число Фибоначчи:", fifth_number)
 print("Двухсотое число Фибоначчи:", two_hundredth_number)
 print("Тысячное число Фибоначчи:", thousandth_number)
 print("Сто тысячное число Фибоначчи:", hundred_thousandth_number)
+
+# Способ 3
+def fibonacci_generator():
+    a, b = 0, 1
+    count = 0
+
+    while True:
+        yield a
+        a, b = b, a + b
+        count += 1
+
+
+# Создание объекта-генератора
+fibonacci_gen = fibonacci_generator()
+
+for i in range(1, 100001):
+    current_number = next(fibonacci_gen)
+
+    if i == 5:
+        fifth_number = current_number
+    elif i == 200:
+        two_hundredth_number = current_number
+    elif i == 1000:
+        thousandth_number = current_number
+    elif i == 100000:
+        hundred_thousandth_number = current_number
+        break  # Прерывание генерации после стотысячного числа
+
+print("Пятое число Фибоначчи:", fifth_number)
+print("Двухсотое число Фибоначчи:", two_hundredth_number)
+print("Тысячное число Фибоначчи:", thousandth_number)
+print("Сто тысячное число Фибоначчи:", hundred_thousandth_number)
